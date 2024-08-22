6c0d1a184aa4dfd4eedec1226cb2cc56ee872ca2
Leonardo Matos <leonardo2matos@icloud.com>
2024-08-22 16:02:13 +0100

    add new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Leonardo Matos - Front end developer",
   description: "portfolio",
+  icons: {
+    icon: "/favicon.ico"
+  }
 };
 
 export default function RootLayout({
